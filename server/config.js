ba503ed4d61e56f13ae5d83fa44b067b767ac375
rafaelreisramos <rrramos@gmail.com>
2022-03-14 15:30:37 -0300

    chore: add env configs

---
+++ server/config.js
@@ -0,0 +1,25 @@
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+const rootDir = join(currentDir, "../")
+const audioDir = join(rootDir, "audio")
+const publicDir = join(rootDir, "public")
+
+export default {
+  port: process.env.PORT || 3000,
+  dir: {
+    root: rootDir,
+    public: publicDir,
+    audio: audioDir,
+    songs: join(audioDir, "songs"),
+    fx: join(audioDir, "fx"),
+  },
+  pages: {
+    home: "home/index.html",
+    controller: "controller/index.html",
+  },
+  location: {
+    home: "/home",
+  },
+}
