3b981142b53c0ce030a91a17287af8343906bc80
rafaelreisramos <rrramos@gmail.com>
2022-03-17 23:52:26 -0300

    fix: create each server as unique

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ import config from "./config.js"
 import server from "./server.js"
 import { logger } from "./util.js"
 
-server
+server()
   .listen(config.port)
   .on("listening", () => logger.info(`Server is running :${config.port}`))
