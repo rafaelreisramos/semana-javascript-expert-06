71b4636d052a4fb252612a8731e1e9ffbb1100fb
rafaelreisramos <rrramos@gmail.com>
2022-03-18 17:12:51 -0300

    test: add additional routes tests

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -110,6 +110,53 @@ describe("#Routes - test site for api response", () => {
     expect(params.response.end).toHaveBeenCalled()
   })
 
+  test("GET /stream?id=123 - it should call createClientStream", async () => {
+    const params = TestUtil.defaultHandleParams()
+    params.request.method = "GET"
+    params.request.url = "/stream"
+    const stream = TestUtil.generateReadableStream(["test"])
+    jest.spyOn(stream, "pipe").mockReturnValue()
+    const onClose = jest.fn()
+    jest
+      .spyOn(Controller.prototype, Controller.prototype.createClientStream.name)
+      .mockReturnValue({
+        onClose,
+        stream,
+      })
+
+    await handler(...params.values())
+    params.request.emit("close")
+
+    expect(params.response.writeHead).toHaveBeenCalledWith(200, {
+      "Content-Type": "audio/mpeg",
+      "Accept-Ranges": "bytes",
+    })
+    expect(Controller.prototype.createClientStream).toHaveBeenCalled()
+    expect(stream.pipe).toHaveBeenCalledWith(params.response)
+    expect(onClose).toHaveBeenCalled()
+  })
+
+  test("POST /controller - should call handleCommand", async () => {
+    const params = TestUtil.defaultHandleParams()
+    params.request.method = "POST"
+    params.request.url = "/controller"
+    const body = {
+      command: "start",
+    }
+    params.request.push(JSON.stringify(body))
+    const result = {
+      ok: "true",
+    }
+    jest
+      .spyOn(Controller.prototype, Controller.prototype.handleCommand.name)
+      .mockResolvedValue(result)
+
+    await handler(...params.values())
+
+    expect(Controller.prototype.handleCommand).toHaveBeenCalledWith(body)
+    expect(params.response.end).toHaveBeenCalledWith(JSON.stringify(result))
+  })
+
   describe("exceptions", () => {
     test("given an inexistent file it should respond with 404", async () => {
       const params = TestUtil.defaultHandleParams()
