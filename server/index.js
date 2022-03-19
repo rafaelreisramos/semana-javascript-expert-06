e48f2d0c45e2deb94015a36d55339fac5636abbd
rafaelreisramos <rrramos@gmail.com>
2022-03-19 20:41:28 -0300

    deploy: add exception handler logs and heroku config

--- server/index.js
+++ server/index.js
@@ -5,3 +5,11 @@ import { logger } from "./util.js"
 server()
   .listen(config.port)
   .on("listening", () => logger.info(`Server is running :${config.port}`))
+
+// avoid crash on unhadled/uncaught errors
+process.on("uncaughtException", (error) =>
+  logger.error(`Uncaught Exception happened: ${error.stack || error} `)
+)
+process.on("unhandledRejection", (error) =>
+  logger.error(`Unhandled Rejection happened: ${error.stack || error} `)
+)
