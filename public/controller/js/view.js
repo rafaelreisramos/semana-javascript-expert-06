5fe5f7cf8d900a41c15905acdbc3857c6a200829
rafaelreisramos <rrramos@gmail.com>
2022-03-19 17:36:13 -0300

    chore: fx buttons click functions and css

--- public/controller/js/view.js
+++ public/controller/js/view.js
@@ -2,8 +2,12 @@ export default class View {
   constructor() {
     this.btnStart = document.getElementById("start")
     this.btnStop = document.getElementById("stop")
+    this.fxButtons = () =>
+      Array.from(document.querySelectorAll("[name=command]"))
+    this.ignoredButtons = new Set(["unassigned"])
     async function onBtnClick() {}
     this.onBtnClick = onBtnClick
+    this.DISABLE_BTN_TIMEOUT = 500
   }
 
   onLoad() {
@@ -12,7 +16,7 @@ export default class View {
   }
 
   changeCommandBtnsVisibility(hide = true) {
-    Array.from(document.querySelectorAll("[name=command]")).forEach((btn) => {
+    this.fxButtons().forEach((btn) => {
       const fn = hide ? "add" : "remove"
       btn.classList[fn]("unassigned")
       function onClickReset() {}
@@ -20,12 +24,62 @@ export default class View {
     })
   }
 
+  notIsUnassigneButton(btn) {
+    const classes = Array.from(btn.classList)
+    return !!!classes.find((classItem) => this.ignoredButtons.has(classItem))
+  }
+
   configureOnBtnClick(fn) {
     this.onBtnClick = fn
   }
 
+  toogleBtnStart(active = true) {
+    if (active) {
+      this.btnStart.classList.add("hidden")
+      this.btnStop.classList.remove("hidden")
+      this.btnStop.onclick = this.onStopClick.bind(this)
+      return
+    }
+    this.btnStart.classList.remove("hidden")
+    this.btnStop.classList.add("hidden")
+  }
+
+  toogleFxButton(classList) {
+    if (!classList.contains("active")) {
+      classList.add("active")
+      return
+    }
+    classList.remove("active")
+  }
+
   async onStartClick({ srcElement: { innerText } }) {
-    const btnText = innerText
-    await this.onBtnClick(btnText)
+    const startCommand = innerText.toLowerCase()
+    await this.onBtnClick(startCommand)
+    this.toogleBtnStart()
+    this.changeCommandBtnsVisibility(false)
+    this.fxButtons()
+      .filter((btn) => this.notIsUnassigneButton(btn))
+      .forEach(this.setupFxAction.bind(this))
+  }
+
+  onStopClick({ srcElement: { innerText } }) {
+    const stopCommand = innerText.toLowerCase()
+    this.toogleBtnStart(false)
+    this.changeCommandBtnsVisibility()
+    return this.onBtnClick(stopCommand)
+  }
+
+  setupFxAction(btn) {
+    btn.onclick = this.onFxClick.bind(this)
+  }
+
+  async onFxClick(btn) {
+    const {
+      srcElement: { classList, innerText },
+    } = btn
+    this.toogleFxButton(classList)
+    const fxCommand = innerText.toLowerCase()
+    await this.onBtnClick(fxCommand)
+    setTimeout(() => this.toogleFxButton(classList), this.DISABLE_BTN_TIMEOUT)
   }
 }
